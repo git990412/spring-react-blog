262b8e30e47daa036ecc41450382962b37146465
git990412 <wjdwn282@gmail.com>
2023-12-16 02:16:09 +0900

    리액트 추가 및 초기설정

---
+++ frontapp/src/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import ErrorPage from './error-page.tsx';
+import { NextUIProvider } from '@nextui-org/react';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <NextUIProvider>
+      <RouterProvider router={router} />
+    </NextUIProvider>
+  </React.StrictMode>,
+)
---
+++ src/main/java/com/ll/medium/domain/member/member/controller/ApiV1MemberController.java
@@ -0,0 +1,10 @@
+package com.ll.medium.domain.member.member.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/v1/member")
+public class ApiV1MemberController {
+
+}
